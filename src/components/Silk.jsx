3d1f8d9eaecf4fa51e387f0566eefded79b6c775
Phillip Stephan <Phillip.Stephan@deutschebahn.com>
2026-02-04 09:23:08 +0100

    silk background

--- src/components/Silk.jsx
+++ src/components/Silk.jsx
@@ -1,3 +1,4 @@
+'use client';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { forwardRef, useRef, useMemo, useLayoutEffect } from 'react';
 import { Color } from 'three';
